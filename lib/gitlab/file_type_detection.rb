ngen-brett/gitlab-ce

CE-specific changes to allow svg design images
This change renames the previous `DANGEROUS_EXT` constant to
`DANGEROUS_IMAGE_EXT`, as it contains only `'svg'`, and allows
developers to allow the inclusion of `'svg'` in their checks by passing
`allow_dangerous_ext: true` to the relevant methods.

https://gitlab.com/gitlab-org/gitlab-ee/issues/12771

lib/gitlab/file_type_detection.rb
@@ -1,7 +1,26 @@
 # frozen_string_literal: true
 
-# File helpers methods.
-# It needs the method filename to be defined.
+# The method `filename` must be defined in classes that use this module.
+#
+# This module is intended to be used as a helper and not a security gate
+# that validates that a file is safe, as it identifies files only by the
+# file extension and not its actual contents.
+#
+# For example, a valid use of this module is in `FileMarkdownLinkBuilder` which
+# renders markdown depending on a file name.
+#
+# In some workflows we don't have the content of the file to detect the
+# real content type. For example, in scenarios where the user can use git,
+# files won't go through our analyzers.
+#
+# In order to mitigate this we use Workhorse to detect the real extension
+# when we serve files with the `SendsBlob` helper methods, and ask Workhorse
+# to set the content type when it serves the file:
+# https://gitlab.com/gitlab-org/gitlab-ce/blob/33e5955/app/helpers/workhorse_helper.rb#L48.
+#
+# Because Workhorse has access to the content when it is downloaded, if
+# the type/extension doesn't match the real type, we adjust the
+# Content-Type and Content-Disposition to the one we get from the detection.
 module Gitlab
   module FileTypeDetection
     IMAGE_EXT = %w[png jpg jpeg gif bmp tiff ico].freeze
@@ -13,22 +32,24 @@ module Gitlab
     VIDEO_EXT = %w[mp4 m4v mov webm ogv].freeze
     # These extension types can contain dangerous code and should only be embedded inline with
     # proper filtering. They should always be tagged as "Content-Disposition: attachment", not "inline".
-    DANGEROUS_EXT = %w[svg].freeze
+    DANGEROUS_IMAGE_EXT = %w[svg].freeze
 
-    def image?
-      extension_match?(IMAGE_EXT)
+    def image?(allow_dangerous_ext: false)
+      safe_match = extension_match?(IMAGE_EXT)
+
+      safe_match || (allow_dangerous_ext && image_with_dangerous_ext?)
+    end
+
+    def image_with_dangerous_ext?
+      extension_match?(DANGEROUS_IMAGE_EXT)
     end
 
     def video?
       extension_match?(VIDEO_EXT)
     end
 
-    def image_or_video?
-      image? || video?
-    end
-
-    def dangerous?
-      extension_match?(DANGEROUS_EXT)
+    def image_or_video?(allow_dangerous_ext: false)
+      image?(allow_dangerous_ext: allow_dangerous_ext) || video?
     end
 
     private

spec/lib/gitlab/file_type_detection_spec.rb
@@ -2,22 +2,72 @@
 require 'spec_helper'
 
 describe Gitlab::FileTypeDetection do
-  def upload_fixture(filename)
-    fixture_file_upload(File.join('spec', 'fixtures', filename))
-  end
+  context 'when class is an uploader' do
+    let(:uploader) do
+      example_uploader = Class.new(CarrierWave::Uploader::Base) do
+        include Gitlab::FileTypeDetection
 
-  describe '#image_or_video?' do
-    context 'when class is an uploader' do
-      let(:uploader) do
-        example_uploader = Class.new(CarrierWave::Uploader::Base) do
-          include Gitlab::FileTypeDetection
-
-          storage :file
-        end
-
-        example_uploader.new
+        storage :file
       end
 
+      example_uploader.new
+    end
+
+    def upload_fixture(filename)
+      fixture_file_upload(File.join('spec', 'fixtures', filename))
+    end
+
+    describe '#image?' do
+      it 'returns true for an image file' do
+        uploader.store!(upload_fixture('dk.png'))
+
+        expect(uploader).to be_image
+      end
+
+      it 'returns false for a video file' do
+        uploader.store!(upload_fixture('video_sample.mp4'))
+
+        expect(uploader).not_to be_image
+      end
+
+      it 'returns false if filename is blank' do
+        uploader.store!(upload_fixture('dk.png'))
+
+        allow(uploader).to receive(:filename).and_return(nil)
+
+        expect(uploader).not_to be_image
+      end
+
+      describe 'when file has a dangerous extension' do
+        before do
+          uploader.store!(upload_fixture('unsanitized.svg'))
+        end
+
+        it 'returns false' do
+          expect(uploader).not_to be_image
+        end
+
+        it 'returns true when passed the `allow_dangerous_ext: true` argument' do
+          expect(uploader.image?(allow_dangerous_ext: true)).to eq(true)
+        end
+      end
+    end
+
+    describe '#image_with_dangerous_ext?' do
+      it 'returns true when image is an svg' do
+        uploader.store!(upload_fixture('unsanitized.svg'))
+
+        expect(uploader).to be_image_with_dangerous_ext
+      end
+
+      it 'returns false image is not dangerous' do
+        uploader.store!(upload_fixture('dk.png'))
+
+        expect(uploader).not_to be_image_with_dangerous_ext
+      end
+    end
+
+    describe '#image_or_video?' do
       it 'returns true for an image file' do
         uploader.store!(upload_fixture('dk.png'))
 
@@ -43,17 +93,81 @@ describe Gitlab::FileTypeDetection do
 
         expect(uploader).not_to be_image_or_video
       end
-    end
 
-    context 'when class is a regular class' do
-      let(:custom_class) do
-        custom_class = Class.new do
-          include Gitlab::FileTypeDetection
+      describe 'when file has a dangerous extension' do
+        before do
+          uploader.store!(upload_fixture('unsanitized.svg'))
         end
 
-        custom_class.new
+        it 'returns false' do
+          expect(uploader).not_to be_image_or_video
+        end
+
+        it 'returns true when passed the `allow_dangerous_ext: true` argument' do
+          expect(uploader.image_or_video?(allow_dangerous_ext: true)).to eq(true)
+        end
+      end
+    end
+  end
+
+  context 'when class is a regular class' do
+    let(:custom_class) do
+      custom_class = Class.new do
+        include Gitlab::FileTypeDetection
       end
 
+      custom_class.new
+    end
+
+    describe '#image?' do
+      it 'returns true for an image file' do
+        allow(custom_class).to receive(:filename).and_return('dk.png')
+
+        expect(custom_class).to be_image
+      end
+
+      it 'returns false for a video file' do
+        allow(custom_class).to receive(:filename).and_return('video_sample.mp4')
+
+        expect(custom_class).not_to be_image
+      end
+
+      it 'returns false if filename is blank' do
+        allow(custom_class).to receive(:filename).and_return(nil)
+
+        expect(custom_class).not_to be_image
+      end
+
+      describe 'when file has a dangerous extension' do
+        before do
+          allow(custom_class).to receive(:filename).and_return('unsanitized.svg')
+        end
+
+        it 'returns false' do
+          expect(custom_class).not_to be_image
+        end
+
+        it 'returns true when passed the `allow_dangerous_ext: true` argument' do
+          expect(custom_class.image?(allow_dangerous_ext: true)).to eq(true)
+        end
+      end
+    end
+
+    describe '#image_with_dangerous_ext?' do
+      it 'returns true when image is an svg' do
+        allow(custom_class).to receive(:filename).and_return('unsanitized.svg')
+
+        expect(custom_class).to be_image_with_dangerous_ext
+      end
+
+      it 'returns false image is not dangerous' do
+        allow(custom_class).to receive(:filename).and_return('dk.png')
+
+        expect(custom_class).not_to be_image_with_dangerous_ext
+      end
+    end
+
+    describe '#image_or_video?' do
       it 'returns true for an image file' do
         allow(custom_class).to receive(:filename).and_return('dk.png')
 
@@ -76,6 +190,20 @@ describe Gitlab::FileTypeDetection do
         allow(custom_class).to receive(:filename).and_return(nil)
 
         expect(custom_class).not_to be_image_or_video
+      end
+
+      describe 'when file has a dangerous extension' do
+        before do
+          allow(custom_class).to receive(:filename).and_return('unsanitized.svg')
+        end
+
+        it 'returns false' do
+          expect(custom_class).not_to be_image_or_video
+        end
+
+        it 'returns true when passed the `allow_dangerous_ext: true` argument' do
+          expect(custom_class.image_or_video?(allow_dangerous_ext: true)).to eq(true)
+        end
       end
     end
   end

cpp597455873/gitlibimport

lib/gitlab/file_type_detection.rb
@@ -1,7 +1,26 @@
 # frozen_string_literal: true
 
-# File helpers methods.
-# It needs the method filename to be defined.
+# The method `filename` must be defined in classes that use this module.
+#
+# This module is intended to be used as a helper and not a security gate
+# that validates that a file is safe, as it identifies files only by the
+# file extension and not its actual contents.
+#
+# For example, a valid use of this module is in `FileMarkdownLinkBuilder` which
+# renders markdown depending on a file name.
+#
+# In some workflows we don't have the content of the file to detect the
+# real content type. For example, in scenarios where the user can use git,
+# files won't go through our analyzers.
+#
+# In order to mitigate this we use Workhorse to detect the real extension
+# when we serve files with the `SendsBlob` helper methods, and ask Workhorse
+# to set the content type when it serves the file:
+# https://gitlab.com/gitlab-org/gitlab-ce/blob/33e5955/app/helpers/workhorse_helper.rb#L48.
+#
+# Because Workhorse has access to the content when it is downloaded, if
+# the type/extension doesn't match the real type, we adjust the
+# Content-Type and Content-Disposition to the one we get from the detection.
 module Gitlab
   module FileTypeDetection
     IMAGE_EXT = %w[png jpg jpeg gif bmp tiff ico].freeze
@@ -13,22 +32,24 @@ module Gitlab
     VIDEO_EXT = %w[mp4 m4v mov webm ogv].freeze
     # These extension types can contain dangerous code and should only be embedded inline with
     # proper filtering. They should always be tagged as "Content-Disposition: attachment", not "inline".
-    DANGEROUS_EXT = %w[svg].freeze
+    DANGEROUS_IMAGE_EXT = %w[svg].freeze
 
-    def image?
-      extension_match?(IMAGE_EXT)
+    def image?(dangerous_exts: false)
+      safe_match = extension_match?(IMAGE_EXT)
+
+      safe_match || (dangerous_exts && image_with_dangerous_ext?)
+    end
+
+    def image_with_dangerous_ext?
+      extension_match?(DANGEROUS_IMAGE_EXT)
     end
 
     def video?
       extension_match?(VIDEO_EXT)
     end
 
-    def image_or_video?
-      image? || video?
-    end
-
-    def dangerous?
-      extension_match?(DANGEROUS_EXT)
+    def image_or_video?(dangerous_exts: false)
+      image?(dangerous_exts: dangerous_exts) || video?
     end
 
     private

spec/lib/gitlab/file_type_detection_spec.rb
@@ -2,22 +2,72 @@
 require 'spec_helper'
 
 describe Gitlab::FileTypeDetection do
-  def upload_fixture(filename)
-    fixture_file_upload(File.join('spec', 'fixtures', filename))
-  end
+  context 'when class is an uploader' do
+    let(:uploader) do
+      example_uploader = Class.new(CarrierWave::Uploader::Base) do
+        include Gitlab::FileTypeDetection
 
-  describe '#image_or_video?' do
-    context 'when class is an uploader' do
-      let(:uploader) do
-        example_uploader = Class.new(CarrierWave::Uploader::Base) do
-          include Gitlab::FileTypeDetection
-
-          storage :file
-        end
-
-        example_uploader.new
+        storage :file
       end
 
+      example_uploader.new
+    end
+
+    def upload_fixture(filename)
+      fixture_file_upload(File.join('spec', 'fixtures', filename))
+    end
+
+    describe '#image?' do
+      it 'returns true for an image file' do
+        uploader.store!(upload_fixture('dk.png'))
+
+        expect(uploader).to be_image
+      end
+
+      it 'returns false for a video file' do
+        uploader.store!(upload_fixture('video_sample.mp4'))
+
+        expect(uploader).not_to be_image
+      end
+
+      it 'returns false if filename is blank' do
+        uploader.store!(upload_fixture('dk.png'))
+
+        allow(uploader).to receive(:filename).and_return(nil)
+
+        expect(uploader).not_to be_image
+      end
+
+      describe 'when file has a dangerous extension' do
+        before do
+          uploader.store!(upload_fixture('unsanitized.svg'))
+        end
+
+        it 'returns false' do
+          expect(uploader).not_to be_image
+        end
+
+        it 'returns true when passed the `dangerous_exts: true` argument' do
+          expect(uploader.image?(dangerous_exts: true)).to eq(true)
+        end
+      end
+    end
+
+    describe '#image_with_dangerous_ext?' do
+      it 'returns true when image is an svg' do
+        uploader.store!(upload_fixture('unsanitized.svg'))
+
+        expect(uploader).to be_image_with_dangerous_ext
+      end
+
+      it 'returns false image is not dangerous' do
+        uploader.store!(upload_fixture('dk.png'))
+
+        expect(uploader).not_to be_image_with_dangerous_ext
+      end
+    end
+
+    describe '#image_or_video?' do
       it 'returns true for an image file' do
         uploader.store!(upload_fixture('dk.png'))
 
@@ -43,17 +93,81 @@ describe Gitlab::FileTypeDetection do
 
         expect(uploader).not_to be_image_or_video
       end
-    end
 
-    context 'when class is a regular class' do
-      let(:custom_class) do
-        custom_class = Class.new do
-          include Gitlab::FileTypeDetection
+      describe 'when file has a dangerous extension' do
+        before do
+          uploader.store!(upload_fixture('unsanitized.svg'))
         end
 
-        custom_class.new
+        it 'returns false' do
+          expect(uploader).not_to be_image_or_video
+        end
+
+        it 'returns true when passed the `dangerous_exts: true` argument' do
+          expect(uploader.image_or_video?(dangerous_exts: true)).to eq(true)
+        end
+      end
+    end
+  end
+
+  context 'when class is a regular class' do
+    let(:custom_class) do
+      custom_class = Class.new do
+        include Gitlab::FileTypeDetection
       end
 
+      custom_class.new
+    end
+
+    describe '#image?' do
+      it 'returns true for an image file' do
+        allow(custom_class).to receive(:filename).and_return('dk.png')
+
+        expect(custom_class).to be_image
+      end
+
+      it 'returns false for a video file' do
+        allow(custom_class).to receive(:filename).and_return('video_sample.mp4')
+
+        expect(custom_class).not_to be_image
+      end
+
+      it 'returns false if filename is blank' do
+        allow(custom_class).to receive(:filename).and_return(nil)
+
+        expect(custom_class).not_to be_image
+      end
+
+      describe 'when file has a dangerous extension' do
+        before do
+          allow(custom_class).to receive(:filename).and_return('unsanitized.svg')
+        end
+
+        it 'returns false' do
+          expect(custom_class).not_to be_image
+        end
+
+        it 'returns true when passed the `dangerous_exts: true` argument' do
+          expect(custom_class.image?(dangerous_exts: true)).to eq(true)
+        end
+      end
+    end
+
+    describe '#image_with_dangerous_ext?' do
+      it 'returns true when image is an svg' do
+        allow(custom_class).to receive(:filename).and_return('unsanitized.svg')
+
+        expect(custom_class).to be_image_with_dangerous_ext
+      end
+
+      it 'returns false image is not dangerous' do
+        allow(custom_class).to receive(:filename).and_return('dk.png')
+
+        expect(custom_class).not_to be_image_with_dangerous_ext
+      end
+    end
+
+    describe '#image_or_video?' do
       it 'returns true for an image file' do
         allow(custom_class).to receive(:filename).and_return('dk.png')
 
@@ -76,6 +190,20 @@ describe Gitlab::FileTypeDetection do
         allow(custom_class).to receive(:filename).and_return(nil)
 
         expect(custom_class).not_to be_image_or_video
+      end
+
+      describe 'when file has a dangerous extension' do
+        before do
+          allow(custom_class).to receive(:filename).and_return('unsanitized.svg')
+        end
+
+        it 'returns false' do
+          expect(custom_class).not_to be_image_or_video
+        end
+
+        it 'returns true when passed the `dangerous_exts: true` argument' do
+          expect(custom_class.image_or_video?(dangerous_exts: true)).to eq(true)
+        end
       end
     end
   end